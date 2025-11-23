072a7b4b57395cf94ebc70dd2b65ea98d4df4985
Fewdev1122 <phumiphatasunee478@gmail.com>
2025-11-23 21:37:52 +0700

    Initial commit: React + Tailwind project

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+export default function Navbar() {
+  return (
+    <nav className="bg-[#0F172A] text-[#fff] flex justify-center h-16">
+      <div className="w-[80%] flex ">
+        <div className="flex justify-between w-full ">
+          <div>
+            <a href="/">Car Market</a>
+          </div>
+          <div>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Car</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
