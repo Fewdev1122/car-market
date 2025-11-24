3f3b318a577fb8295b72455f1136d3663ee17c21
Fewdev1122 <phumiphatasunee478@gmail.com>
2025-11-24 23:31:15 +0700

    add style in card Section Recommend

--- src/components/recommend.jsx
+++ src/components/recommend.jsx
@@ -1,35 +1,50 @@
 import React from "react";
 import Cars from "../data/cars.json";
+import { NavLink } from "react-router-dom";
+import IcDate from "./imgs/formkit--date.svg";
 
 
 export default function Recommend() {
   return (
-    <div>
-      <div>
-        <span>รถยนต์แนะนำ</span>
-        <p>รถยนต์คุณภาพที่ได้รับการคัดสรรมาเป็นพิเศษ</p>
-      </div>
-      
-        { Cars.slice(0, 4).map((Cars) => (
-          <div>
-            <div>
-              <div>
-                <div>
-                  <img src={Cars.image} alt={Cars.model} width="250" height="100"/>
+    <div className=" bg-[#0F172A] py-[4rem] ">
+      <div className="container mx-auto">
+        <div className="flex flex-col justify-center items-center space-y-2  mb-10">
+          <span className="text-white">รถยนต์แนะนำ</span>
+          <p className="text-gray-400">รถยนต์คุณภาพที่ได้รับการคัดสรรมาเป็นพิเศษ</p>
+        </div>
+
+
+        <div >
+          <div className="grid gap-1 grid-cols-[repeat(auto-fit,minmax(250px,1fr))] place-items-center">
+            {Cars.slice(0, 4).map((Cars) => (
+              <div key={Cars.id} className="flex flex-col gap-2 bg-[#1E293B] rounded-xl w-[240px] border border-gray-700 xl:w-[300px] 2xl:w-[320px] cursor-pointer group transition-shadow duration-300 hover:shadow-[0_10px_20px_rgba(59,130,246,0.3)]">
+                <div className="w-full aspect-[16/9] overflow-hidden rounded-t-xl">
+                  <img src={Cars.image} alt={Cars.model} className="w-full h-full object-cover transform transition-transform duration-300 group-hover:scale-105" />
                 </div>
-                <div>
-                  <p>{Cars.brand}</p>
-                  <p>{Cars.model}</p>
-                  <div>
-                    <p>{Cars.year}</p>
-                    <p>{Cars.mileage}</p>
+                <div className="p-4 pt-[1.75rem] ">
+                  <p className="text-blue-400 text-sm mb-1">{Cars.brand}</p>
+                  <p className="text-[#fff] mb-2">{Cars.model}</p>
+                  <div className="flex gap-3 text-sm text-gray-400 mb-2">
+                    <div className="flex item-center gap-1">
+                      <img src={IcDate} alt="Loading" className="w-3.5 text-bold" />
+                      <p >{Cars.year}</p>
+                    </div>
+
+                    <p>{Cars.mileage} km</p>
                   </div>
-                  <p>{Cars.price}</p>
+                  <p className="text-blue-500 ">฿{Cars.price}</p>
+                  <div className="flex justify-center mt-8 w-full ">
+                    <NavLink className="w-full" to="">
+                      <button className="bg-[#3B82F6] text-white text-sm py-1 px-9 w-full rounded-lg xl:py-2 hover:bg-[#60A5FA]">ดูรายละเอียด</button>
+                    </NavLink>
+                  </div>
                 </div>
+
               </div>
-            </div>
+            ))}
           </div>
-      ))}
+        </div>
+      </div>
     </div>
   )
 }
