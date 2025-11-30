eb6da0bed9d649ac3d29a1f96e84764da3175a64
Fewdev1122 <phumiphatasunee478@gmail.com>
2025-11-30 19:23:34 +0700

    cart cars

--- src/components/cartCar.jsx
+++ src/components/cartCar.jsx
@@ -1,5 +1,7 @@
-
 import Cars from "../data/cars.json";
+import { NavLink } from "react-router-dom";
+import IcDate from "./imgs/formkit--date.svg";
+import IcMileage from "./imgs/formkit--mileage.svg";
 
 export default function CartCar() {
   return (
@@ -64,6 +66,38 @@ export default function CartCar() {
             <button type="reset">ล้างตัวกรอง</button>
           </div>
         </form>
+          <div >
+          <div className="grid gap-1 grid-cols-[repeat(auto-fit,minmax(250px,1fr))] place-items-center">
+            {Cars.map((Cars) => (
+              <div key={Cars.id} className="flex flex-col gap-2 bg-[#1E293B] rounded-xl w-[240px] border border-gray-700 xl:w-[300px] 2xl:w-[320px] cursor-pointer group transition-shadow duration-300 hover:shadow-[0_10px_20px_rgba(37,99,235,0.19)]">
+                <div className="w-full aspect-[16/9] overflow-hidden rounded-t-xl">
+                  <img src={Cars.image} alt={Cars.model} className="w-full h-full object-cover transform transition-transform duration-300 group-hover:scale-105" />
+                </div>
+                <div className="p-4 pt-[1.75rem] ">
+                  <p className="text-blue-400 text-sm mb-1">{Cars.brand}</p>
+                  <p className="text-[#fff] mb-2">{Cars.model}</p>
+                  <div className="flex gap-3 text-sm text-gray-400 mb-2">
+                    <div className="flex item-center gap-1">
+                      <img src={IcDate} alt="Loading" className="w-3.5 " />
+                      <p >{Cars.year}</p>
+                    </div>
+                    <div className="flex item-center gap-1">
+                      <img src={IcMileage} alt="Loading" className="w-4.5 " />
+                      <p>{Cars.mileage} km</p>
+                    </div>
+                  </div>
+                  <p className="text-blue-500 ">฿{Cars.price}</p>
+                  <div className="flex justify-center mt-8 w-full ">
+                    <NavLink className="w-full"  to={`/detail/${Cars.id}`} state={Cars}>
+                      <button className="bg-[#3B82F6] text-white text-sm py-1 px-9 w-full rounded-lg xl:py-2 hover:bg-[#60A5FA]">ดูรายละเอียด</button>
+                    </NavLink>
+                  </div>
+                </div>
+
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   )
